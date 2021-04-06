985594f6b81a4c8be4ede2e2f77321c503dac8fc
Vilivi <vilivi.b@gmail.com>
2021-04-06 10:02:54 +0200

    Ajout des meilleures ventes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
-use App\Classe\Mail;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * @Route("/", name="home")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $products = $this->em->getRepository(Product::class)->findByIsBest(1);
+        return $this->render('home/index.html.twig', [
+            'products' => $products
+        ]);
     }
 }
